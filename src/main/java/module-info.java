4360eb18540729d1c3a670e657d546795e702aad
Youssef Ali Rashidi Ali <es.yousef.rashidy2023@alexu.edu.eg>
2026-02-24 13:58:59 +0200

    Initial Commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module org.example.sortingalgorithmsvisualization {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens org.example.sortingalgorithmsvisualization to javafx.fxml;
+    exports org.example.sortingalgorithmsvisualization;
+}
