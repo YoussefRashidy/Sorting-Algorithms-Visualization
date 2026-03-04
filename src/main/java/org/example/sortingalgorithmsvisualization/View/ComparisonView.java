54a9c2cc993b680c90d37915bcf15716e8f03ef8
Youssef Ali Rashidi Ali <es.yousef.rashidy2023@alexu.edu.eg>
2026-03-04 03:16:33 +0200

    basic styles for comparison view

--- src/main/java/org/example/sortingalgorithmsvisualization/View/ComparisonView.java
+++ src/main/java/org/example/sortingalgorithmsvisualization/View/ComparisonView.java
@@ -37,27 +37,28 @@ public class ComparisonView extends VBox {
         table.setPrefHeight(bounds.getHeight());
         table.setPrefWidth(bounds.getWidth());
         table.setStyle("""
-                -fx-background-color: #0d1117;
-                -fx-border-color: #21262d;
-                -fx-border-width: 1;
+                -fx-background-color: white;
+                -fx-border-color: #d1d5db;
+                -fx-border-width: 2;
                 -fx-table-cell-border-color: #21262d;
                 -fx-font-size: 13px;
-                -fx-selection-bar: #1f6feb;
                 -fx-text-fill: White;
                 """);
 
         Platform.runLater(() -> {
             table.lookupAll(".column-header").forEach(node -> {
                 node.setStyle("""
-                            -fx-background-color: #161b22;
-                            -fx-text-fill: #ffffff;
+                            -fx-background-color: #f3f4f6;
+                            -fx-text-fill: #111827;
                             -fx-font-weight: bold;
                             -fx-font-size: 13px;
                             -fx-pref-height: 50px;
-                            -fx-border-color: #313244;
-                            -fx-border-width: 0 1 2 0;
+                            -fx-border-color: #d1d5db;
+                            -fx-border-width: 1 1 2 1;
                             -fx-alignment: CENTER-LEFT;
-                            -fx-padding: 0 0 0 10;
+                            -fx-padding: 5 5 5 5;
+                            -fx-border-radius: 3px;
+                            -fx-effect: dropshadow(gaussian, rgba(0,0,0,0.2), 3, 0.3, 0, 2);
                         """);
             });
         });
@@ -65,39 +66,39 @@ public class ComparisonView extends VBox {
         TableColumn<ComparisonStat, String> sortingAlgorithm = new TableColumn<>("Algorithm");
         sortingAlgorithm.setCellValueFactory(new PropertyValueFactory<>("sortingAlgorithm"));
         table.setFixedCellSize(50);
-        styleColumn(sortingAlgorithm, "#89b4fa", "CENTER-LEFT");
+        styleColumn(sortingAlgorithm);
 
         TableColumn<ComparisonStat, String> arrayGenerationMode = new TableColumn<>("Generation Mode");
         arrayGenerationMode.setCellValueFactory(new PropertyValueFactory<>("generationMode"));
-        styleColumn(arrayGenerationMode, "#a6e3a1", "CENTER");
+        styleColumn(arrayGenerationMode);
 
         TableColumn<ComparisonStat, Integer> arraySize = new TableColumn<>("Array Size");
         arraySize.setCellValueFactory(new PropertyValueFactory<>("arraySize"));
-        styleColumn(arraySize, "#f9e2af", "CENTER");
+        styleColumn(arraySize);
 
         TableColumn<ComparisonStat, Integer> numberOfRuns = new TableColumn<>("Runs");
         numberOfRuns.setCellValueFactory(new PropertyValueFactory<>("numberOfruns"));
-        styleColumn(numberOfRuns, "#fab387", "CENTER");
+        styleColumn(numberOfRuns);
 
         TableColumn<ComparisonStat, Double> averageTime = new TableColumn<>("Avg Time (ns)");
         averageTime.setCellValueFactory(new PropertyValueFactory<>("averageTime"));
-        styleColumn(averageTime, "#eba0ac", "CENTER-RIGHT");
+        styleColumn(averageTime);
 
         TableColumn<ComparisonStat, Double> minTime = new TableColumn<>("Min Time (ns)");
         minTime.setCellValueFactory(new PropertyValueFactory<>("minTime"));
-        styleColumn(minTime, "#94e2d5", "CENTER-RIGHT");
+        styleColumn(minTime);
 
         TableColumn<ComparisonStat, Double> maxTime = new TableColumn<>("Max Time (ns)");
         maxTime.setCellValueFactory(new PropertyValueFactory<>("maxTime"));
-        styleColumn(maxTime, "#f38ba8", "CENTER-RIGHT");
+        styleColumn(maxTime);
 
         TableColumn<ComparisonStat, Long> numOfComparisons = new TableColumn<>("Comparisons");
         numOfComparisons.setCellValueFactory(new PropertyValueFactory<>("numOfComparison"));
-        styleColumn(numOfComparisons, "#b4befe", "CENTER-RIGHT");
+        styleColumn(numOfComparisons);
 
         TableColumn<ComparisonStat, Long> numOfInterchanges = new TableColumn<>("Interchanges");
         numOfInterchanges.setCellValueFactory(new PropertyValueFactory<>("numOfInterchange"));
-        styleColumn(numOfInterchanges, "#f5c2e7", "CENTER-RIGHT");
+        styleColumn(numOfInterchanges);
 
         table.getColumns().addAll(sortingAlgorithm, arrayGenerationMode, arraySize, averageTime, minTime, maxTime, numOfInterchanges, numOfComparisons, numberOfRuns);
 
@@ -114,26 +115,25 @@ public class ComparisonView extends VBox {
         this.getChildren().add(table);
     }
 
-    private <T> void styleColumn(TableColumn<ComparisonStat, T> col, String color, String alignment) {
+    private <T> void styleColumn(TableColumn<ComparisonStat, T> col) {
         col.setCellFactory(c -> new TableCell<>() {
             @Override
             protected void updateItem(T item, boolean empty) {
                 super.updateItem(item, empty);
                 if (empty || item == null) {
                     setText(null);
-                    setStyle("-fx-background-color: transparent;");
+                    setStyle("");
                 } else {
                     setText(item.toString());
-                    String rowBg = getIndex() % 2 == 0 ? "#0d1117" : "#161b22";
                     setStyle(
-                            "-fx-text-fill: " + color + ";" +
-                            "-fx-alignment: " + alignment + ";" +
-                            "-fx-font-size: 12px;" +
-                            "-fx-padding: 0 12 0 12;" +
-                            "-fx-background-color: " + rowBg + ";" +
-                            "-fx-font-weight: bold;" +
-                            "-fx-border-color: #21262d;" +
-                            "-fx-border-width: 0 0 1 0;"
+                            "-fx-text-fill: " + "#111827" + ";" +
+                                    "-fx-alignment: " + "Center" + ";" +
+                                    "-fx-font-size: 12px;" +
+                                    "-fx-padding: 0 12 0 12;" +
+                                    "-fx-font-weight: bold;" +
+                                    "-fx-border-color: #d1d5db;" +
+                                    "-fx-border-width: 1 1 1 1;" +
+                                    "-fx-border-radius: 3px;"
                     );
                 }
             }
